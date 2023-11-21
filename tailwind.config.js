2c604fe97bf2af6adb19c286bdd86c26a8dd7961
FIZITI <110907574+FIZITI@users.noreply.github.com>
2023-11-21 23:22:41 +0300

    adapriv add initiall
    adapriv add initiall

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'lgl': {'max': '1599px'},
+        'ldd': {'max': '1299px'},
+        'lmd': {'max': '980px'},
+        'sml': {'max': '480px'},
+      },
+    },
   },
   plugins: [],
 }
