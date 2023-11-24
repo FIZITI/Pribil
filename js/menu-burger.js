e0054103f020f11a0f5554f9a670159d80329e47
FIZITI <110907574+FIZITI@users.noreply.github.com>
2023-11-24 18:16:28 +0300

    menu-burger outside
    menu-burger outside

--- js/menu-burger.js
+++ js/menu-burger.js
@@ -1,18 +1,19 @@
 const iconMenu = document.getElementById('menu-burger-icon');
-const opacityBody = document.getElementById('opacity-block1');
-const opacityBody2 = document.getElementById('opacity-block2');
 const menuBurger = document.querySelector('.menu-burger');
 iconMenu.addEventListener("click", function (e) {
     menuBurger.classList.toggle('_active');
-    iconMenu.classList.toggle('opacity-body');
-    opacityBody.classList.toggle('opacity-body');
-    opacityBody2.classList.toggle('opacity-body');
+    document.body.classList.toggle('opacity-body');
 });
 
 const iconClose = document.getElementById('burger-close');
 iconClose.addEventListener("click", function (e) {
     menuBurger.classList.toggle('_active');
-    iconMenu.classList.toggle('opacity-body');
-    opacityBody.classList.toggle('opacity-body');
-    opacityBody2.classList.toggle('opacity-body');
+    document.body.classList.toggle('opacity-body');
+});
+
+document.addEventListener("mouseup", function(event) {
+    if (menuBurger.classList.contains('_active') && !menuBurger.contains(event.target)) {
+        menuBurger.classList.toggle('_active');
+        document.body.classList.toggle('opacity-body');
+    }
 });
